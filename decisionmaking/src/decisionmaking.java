c63e22594224fd1d93b84e352175c630e90acd71
UsamaAhmadTs <usama.ahmad@techno-soft.com>
2023-08-03 18:47:40 +0500

    loops

---
+++ decisionmaking/src/decisionmaking.java
@@ -0,0 +1,72 @@
+public class decisionmaking {
+    // Java program to illustrate if-else-if ladder
+        public static void main(String[] args)
+        {
+            int i = 20;
+
+            if (i == 10)
+                System.out.println("i is 10");
+            else if (i == 15)
+                System.out.println("i is 15");
+            else if (i == 20)
+                System.out.println("i is 20");
+            else
+                System.out.println("i is not present");
+        }
+    }
+
+
+
+//public class decisionmaking {
+//    public static void main (String[] args) {
+//        int num=10;
+//        switch(num){
+//            case 5 : System.out.println("It is 5");
+//                break;
+//            case 10 : System.out.println("It is 10");
+//                break;
+//            case 15 : System.out.println("It is 15");
+//                break;
+//            case 20 : System.out.println("It is 20");
+//                break;
+//            default: System.out.println("Not present");
+//
+//        }
+//    }
+//}
+
+
+// Java program to illustrate using
+// continue in an if statement
+//public class decisionmaking {
+//    public static void main(String args[])
+//    {
+//        for (int i = 0; i < 10; i++) {
+//            // If the number is even
+//            // skip and continue
+//            if (i % 2 == 0)
+//                continue;
+//
+//            // If number is odd, print it
+//            System.out.print(i + " ");
+//        }
+//    }
+//}
+
+
+// Java program to illustrate using return
+//public class decisionmaking{
+//    public static void main(String args[])
+//    {
+//        boolean t = true;
+//        System.out.println("Before the return.");
+//
+//        if (t)
+//            return;
+//
+//        // Compiler will bypass every statement
+//        // after return
+//        System.out.println("This won't execute.");
+//    }
+//}
+
